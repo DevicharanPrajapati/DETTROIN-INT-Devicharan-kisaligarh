9a07c786901c123afdea30388f8a14c27cbfb544
Devicharan Prajapati <devicharan2724@gmail.com>
2026-07-28 01:24:46 +0530

    Contact page created with Google map and form also i used local storage for form data and i handled form using useState and eventlistners

---
+++ src/pages/ContactPage/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactSection from '../../components/Contact/ContactHero'
+
+const ContactPage = () => {
+  return (
+    <div className="pt-10">
+      <ContactSection />
+    </div>
+  )
+}
+
+export default ContactPage
